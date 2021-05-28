d1194d09a8ce16f6cf58a88fc97b0ec9c8259322
Erich Purpur <epurpur@gmail.com>
2021-05-27 20:46:19 -0400

    updated seeds file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,25 +1,62 @@
+// const sequelize = require('../config/connection');
+// const { User, Streetlights } = require('../models');
+
+// // const userData = require('./userData.json');
+// const streetlightsData = require('./streetlights.json');
+
+// const seedDatabase = async () => {
+//   await sequelize.sync({ force: true });
+
+//   const users = await User.bulkCreate(userData, {
+//     individualHooks: true,
+//     returning: true,
+//   });
+
+//   for (const streetlights of streetlightsData) {
+//     await Streetlights.create({
+//       ...streetlights,
+//       user_id: users[Math.floor(Math.random() * users.length)].id,
+//     });
+//   }
+
+//   process.exit(0);
+// };
+
+// seedDatabase();
+
+
+
 const sequelize = require('../config/connection');
-const { User, Streetlights } = require('../models');
+const { Streetlight  } = require('../models');
 
-// const userData = require('./userData.json');
-const streetlightsData = require('./streetlights.json');
+const streetlightData = require('./streetlights.json');
 
-const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+// mapping our data to a better format
+const newData = streetlightData.features.map((item) => {
+  
+  const { WATTS, DECAL_COLO, DECAL_NUMB, MOUNT_HEIG, OWNER, INSTALL_DA, STYLE, WORK_EFFEC, LUMENS, CONTRACT_N, NOM_VOLT, BASE_COLO } = item.properties
 
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
-
-  for (const streetlights of streetlightsData) {
-    await Streetlights.create({
-      ...streetlights,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
+  const obj = {
+    WATTS,
+    DECAL_COLO,
+    DECAL_NUMB,
+    MOUNT_HEIG,
+    OWNER,
+    INSTALL_DA,
+    STYLE,
+    WORK_EFFEC,
+    LUMENS,
+    CONTRACT_C,
+    NOM_VOLT,
+    BASE_COLO,
+    LATITUDE: item.geometry.coordinates[1],
+    LONGITUDE: item.geometry.coordinates[0],
   }
 
-  process.exit(0);
-};
+  return obj
 
-seedDatabase();
+});
+
+
+MOUNT HEIG
+
