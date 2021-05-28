e31211018f3a86238b9951635fda397ff7c5d22b
Christopher Zacharias <78880287+ChrisZ304@users.noreply.github.com>
2021-05-27 22:08:22 -0400

    Revert "fixing branches"

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const { Streetlight } = require('../../models');
+const { Project } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newStreetlight = await Streetlight.create({
+    const newProject = await Project.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newStreetlight);
+    res.status(200).json(newProject);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,19 +17,19 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const streetlightData = await Streetlight.destroy({
+    const projectData = await Project.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!streetlightData) {
+    if (!projectData) {
       res.status(404).json({ message: 'No project found with this id!' });
       return;
     }
 
-    res.status(200).json(streetlightData);
+    res.status(200).json(projectData);
   } catch (err) {
     res.status(500).json(err);
   }
--- models/index.js
+++ models/index.js
@@ -1,12 +1,6 @@
 const User = require('./User');
-<<<<<<< HEAD
-const Streetlight = require('./Streetlight');
-
-module.exports = { User, Streetlight };
-=======
 const Streetlights = require('./Streetlights');
 
 
 
 module.exports = {  Streetlights , User };
->>>>>>> 00728c4547120f23d13834b0b5166d9b970d306e
