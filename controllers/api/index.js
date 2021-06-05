dd6ef1707e96476c05a08c6fb0db6beef7799c7d
shaimaJobran <shaimajobran222gmail.com>
2021-06-05 11:53:17 -0400

    userroutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./apiRoutes');
-
+const userRoutes=require('./userRoutes');
 router.use('/', apiRoutes);
+router.use('/user',userRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const htmlRoutes = require('./htmlRoutes');
 
+
 router.use('/', htmlRoutes);
 router.use('/api', apiRoutes);
+
 
 module.exports = router;
